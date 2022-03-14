2cb5dfa631fab4cb0c7ad85aa540e62d9d58235c
Gustavo Barbosa Castilho <gustavobcx@gmail.com>
2022-03-13 22:48:34 -0300

    add schema validation to config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,15 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      }),
       // ignoreEnvFile: true,
       // envFilePath: '.environment',
     }),
