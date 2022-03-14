9bd50bd5354276bfced4641430df27b29dc8ed99
Gustavo Barbosa Castilho <gustavobcx@gmail.com>
2022-03-13 21:59:12 -0300

    add class providers conditional

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,12 +7,23 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
 // class MockCoffeesService {}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
     {
       provide: COFFEE_BRANDS,
       useValue: ['buddys brew', 'nescafe'],
